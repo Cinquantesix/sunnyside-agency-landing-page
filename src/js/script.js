df02275aa930638e4514f03c9c18d67c7e7a6a7a
Cinquantesix <hello@cinquantesix.ch>
2022-08-24 15:53:07 +0200

    Add mobile menu with styles and function

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
-console.log('yolo');
+let navReveal = document.getElementById("sunnyside__bg-menu");
+
+navReveal.addEventListener("click", function( event ) {
+  
+  var navmenu = document.getElementById("sunnyside__bg-menu");
+
+  if (navmenu.classList.contains('nav__open')) {
+    document.getElementById("sunnyside__bg-menu").classList.remove("nav__open");
+    document.getElementById("sunnyside__nav").classList.remove("sunnyside__nav-reveal");
+  } else {
+    document.getElementById("sunnyside__bg-menu").classList.add("nav__open");
+    document.getElementById("sunnyside__nav").classList.add("sunnyside__nav-reveal");
+  }
+  
+}, false);
